ac93477c5017ae3bc612e0668b8bb952e630a9f7
Juan Mattos <juan.mattos@rootstrap.com>
2022-02-15 11:33:57 -0300

    Fix css issue

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
+// StylesProvider --> here to prevent classname collision in prod
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
